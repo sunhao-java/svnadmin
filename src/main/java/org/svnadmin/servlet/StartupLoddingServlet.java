331cb6dcc492874035a4819ea56f5fca4ab85663
孙昊 <sunhao@xiniaoyun.com>
2019-04-17 21:00:54 +0800

    修改

--- src/main/java/org/svnadmin/servlet/StartupLoddingServlet.java
+++ src/main/java/org/svnadmin/servlet/StartupLoddingServlet.java
@@ -1,7 +1,10 @@
 package org.svnadmin.servlet;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.svnadmin.common.util.HttpUtils;
 import org.svnadmin.common.util.PropUtils;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,9 +31,23 @@ public class StartupLoddingServlet extends HttpServlet {
         String sysName = HttpUtils.urlDecode(PropUtils.get("setting.sys_name"), "utf-8");
         context.setAttribute("sysName", sysName);
         //设置系统应用根目录
-        context.setAttribute("assetsPath", "/" + sysPath + "/assets");
-        context.setAttribute("framePath", "/" + sysPath + "/assets/hui");
-        context.setAttribute("appPath", "/" + sysPath + "/assets/admin");
+        String prefix = "";
+        if (StringUtils.isBlank(sysPath) || StringUtils.equals("/", sysPath)) {
+            // 空 或者 /
+            prefix = "/";
+        }
+        if (!StringUtils.endsWith(sysPath, "/")) {
+            // 不是/ 结尾的
+            prefix = sysPath + "/";
+        }
+        if (!StringUtils.startsWith(sysPath, "/")) {
+            // 不是/ 开头的
+            prefix = "/" + sysPath;
+        }
+
+        context.setAttribute("assetsPath", prefix + "assets");
+        context.setAttribute("framePath", prefix + "assets/hui");
+        context.setAttribute("appPath", prefix + "assets/admin");
         logger.info("load setting finish，the servletContextName is " + context.getServletContextName());
     }
 }
