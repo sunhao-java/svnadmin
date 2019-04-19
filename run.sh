698a453aa375a5d520ed4a1f90adba079ab3be30
孙昊 <sunhao@xiniaoyun.com>
2019-04-19 08:54:10 +0800

    修改问题+代码与镜像分离

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 启动tomcat
-/usr/local/tomcat/bin/startup.sh
-# 启动svn
-/usr/bin/svnserve --daemon --foreground --root /var/opt/svn
--- src/main/java/org/svnadmin/util/interceptor/AdminInterceptor.java
+++ src/main/java/org/svnadmin/util/interceptor/AdminInterceptor.java
@@ -1,11 +1,7 @@
 package org.svnadmin.util.interceptor;
 
-import com.trilead.ssh2.Session;
-import jdk.nashorn.internal.ir.debug.JSONWriter;
-import jdk.nashorn.internal.parser.JSONParser;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.util.JSONPObject;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.svnadmin.common.annotation.AdminAuthPassport;
@@ -13,7 +9,6 @@ import org.svnadmin.common.annotation.AuthPassport;
 import org.svnadmin.common.entity.PushMsg;
 import org.svnadmin.common.util.HttpUtils;
 import org.svnadmin.common.util.PropUtils;
-import org.svnadmin.entity.Usr;
 import org.svnadmin.util.JSONUtils;
 import org.svnadmin.util.SessionUtils;
 
@@ -42,38 +37,58 @@ public class AdminInterceptor extends HandlerInterceptorAdapter {
         HttpSession session = request.getSession();
         HandlerMethod auth = (HandlerMethod) handler;
         AuthPassport authPassport = auth.getMethodAnnotation(AuthPassport.class);
+        String prefix = getPrefix(PropUtils.get("setting.sys_path"));
         if (null != authPassport) {
             if (SessionUtils.isLogin(request)) {
                 //去控制台页面
-                response.sendRedirect("/" + PropUtils.get("setting.sys_path") + "/" + CONSOLE_URL);
+                response.sendRedirect(prefix + CONSOLE_URL);
                 return false;
             }
-        }else{
+        } else {
             //是否已经登录
             if (!SessionUtils.isLogin(request)) {
                 //去登录页面
-                response.sendRedirect("/" + PropUtils.get("setting.sys_path") + "/" + LOGIN_URL);
+                response.sendRedirect(prefix + LOGIN_URL);
                 return false;
             }
         }
         //鉴权
         AdminAuthPassport adminAuth = auth.getMethodAnnotation(AdminAuthPassport.class);
-        if(null != adminAuth){
-            if(!SessionUtils.hasAdminRight(session)){
-                if(HttpUtils.isAjaxRequest(request)){
+        if (null != adminAuth) {
+            if (!SessionUtils.hasAdminRight(session)) {
+                if (HttpUtils.isAjaxRequest(request)) {
                     PushMsg authMsg = new PushMsg("对不起，你无权访问该功能", false);
                     PrintWriter writer = response.getWriter();
                     writer.println(JSONUtils.toJson(authMsg));
                     writer.flush();
                     writer.close();
-                }else{
+                } else {
                     //无权限页面
-                    request.getRequestDispatcher("/WEB-INF/views/common/not_auth.jsp").forward(request,response);
+                    request.getRequestDispatcher("/WEB-INF/views/common/not_auth.jsp").forward(request, response);
                 }
                 return false;
             }
         }
         return true;
+    }
+
+    private String getPrefix(String sysPath) {
+        //设置系统应用根目录
+        String prefix = "";
+        if (StringUtils.isBlank(sysPath) || StringUtils.equals("/", sysPath)) {
+            // 空 或者 /
+            prefix = "/";
+        }
+        if (!StringUtils.endsWith(sysPath, "/")) {
+            // 不是/ 结尾的
+            prefix = sysPath + "/";
+        }
+        if (!StringUtils.startsWith(sysPath, "/")) {
+            // 不是/ 开头的
+            prefix = "/" + sysPath;
+        }
+
+        return prefix;
     }
 
 }
